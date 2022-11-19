4f59dbe5b3de218e527b9df7fe354e6e14059046
Ivan Ng <ivan.ng0515@gmail.com>
2022-11-19 20:31:03 +0800

    changed pos_factor to possible_factor

--- projects/primenum/src/main.rs
+++ projects/primenum/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() { 
-    let mut pos_factor: i32 = 1;
+    let mut possible_factor: i32 = 1;
     let mut how_factors: u32 = 0;
     loop {
         let mut usr_num = String::new();
@@ -13,13 +13,13 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        while usr_num - pos_factor != -1 {
-            if usr_num % pos_factor != 0 {
-                pos_factor += 1;
+        while usr_num - possible_factor != -1 {
+            if usr_num % possible_factor != 0 {
+                possible_factor += 1;
             }
-            if usr_num % pos_factor == 0 {
+            if usr_num % possible_factor == 0 {
                 how_factors += 1;
-                pos_factor += 1;
+                possible_factor += 1;
             }
         }
         if how_factors == 2 {

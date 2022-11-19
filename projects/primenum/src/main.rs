a532d1fb41d4225c0b11725e024669e7b324e920
Ivan Ng <ivan.ng0515@gmail.com>
2022-11-19 20:13:16 +0800

    looping through the program and error handling

--- projects/primenum/src/main.rs
+++ projects/primenum/src/main.rs
@@ -1,28 +1,32 @@
 use std::io;
 
-fn main() {
-    println!("Insert a number: ");
-    let mut usr_num = String::new();
+fn main() { 
     let mut pos_factor: i32 = 1;
     let mut how_factors: u32 = 0;
-
-    io::stdin()
-        .read_line(&mut usr_num)
-        .expect("Failed to read line");
-    let usr_num: i32 = usr_num.trim().parse().expect("Please type a number");
-    while usr_num - pos_factor != -1 {
-        if usr_num % pos_factor != 0 {
-            pos_factor += 1;
+    loop {
+        let mut usr_num = String::new();
+        println!("Insert a number: ");
+        io::stdin()
+            .read_line(&mut usr_num)
+            .expect("Failed to read line");
+        let usr_num: i32 = match usr_num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        while usr_num - pos_factor != -1 {
+            if usr_num % pos_factor != 0 {
+                pos_factor += 1;
+            }
+            if usr_num % pos_factor == 0 {
+                how_factors += 1;
+                pos_factor += 1;
+            }
         }
-        if usr_num % pos_factor == 0 {
-            how_factors += 1;
-            pos_factor += 1;
+        if how_factors == 2 {
+            println!("Is Prime");
         }
-    }
-    if how_factors == 2 {
-        println!("Is Prime");
-    }
-    else {
-        println!("Not Prime");
-    }
+        else {
+            println!("Not Prime");
+        }
+    } 
 }

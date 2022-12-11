cbdace68da03b0226dd029e67ae4de4f38c880e2
ivann <ivan.ng0515@gmail.com>
2022-12-11 21:51:24 +0800

    started calculator project

---
+++ projects/calc/src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() {
+	let mut usr_choice = String::new();
+    println!("Select your option:\n (add/subtract/multiply/divide)");
+    io::stdin()
+    	.read_line(&mut usr_choice)
+    	.expect("Failed to read line");
+}

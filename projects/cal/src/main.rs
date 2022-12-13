4f955f459dc249ad9e03d5efa7af814b2782ebeb
ivann <ivan.ng0515@gmail.com>
2022-12-13 19:21:04 +0800

    changed to cal program

---
+++ projects/cal/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+use chrono;
+
+fn get_date(cmd: String) {
+	println!("{}", cmd)
+}
+fn main() {
+	let mut usr_cmd = String::new();
+	io::stdin()
+    	.read_line(&mut usr_cmd)
+    	.expect("Failed to read line");
+	get_date(usr_cmd);
+}

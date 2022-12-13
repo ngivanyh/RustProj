f4426d0ebe34894147aaab8a80479ec5ec561ed7
ivann <ivan.ng0515@gmail.com>
2022-12-13 17:44:31 +0800

    commit

--- projects/calc/src/main.rs
+++ projects/calc/src/main.rs
@@ -4,8 +4,6 @@ fn calc() {
 	println!("Hello")
 }
 fn main() {
-	let mut usr_choice = String::new();
-    println!("Select your option:\n (add/subtract/multiply/divide)");
     io::stdin()
     	.read_line(&mut usr_choice)
     	.expect("Failed to read line");

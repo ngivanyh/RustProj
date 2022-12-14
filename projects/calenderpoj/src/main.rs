e47a86d2671e0d4215fb7296155700746df6f3cc
ivann <ivan.ng0515@gmail.com>
2022-12-14 18:37:38 +0800

    commit

--- projects/calenderpoj/src/main.rs
+++ projects/calenderpoj/src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
 
+fn cal(cmd: String) {
+    println!("{}", cmd);
+}
 fn main() {
     let mut usr_in = String::new();
     io::stdin()
-	.read_line(&mut usr_in)
-	.expect("Failed to read line");
-    println!("{}", usr_in);
+        .read_line(&mut usr_in)
+        .expect("Failed to read line");
+    cal(usr_in);
 }

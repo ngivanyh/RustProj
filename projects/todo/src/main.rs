f96e70305f2435f17d8293226c82beacd328a93b
ivann <ivan.ng0515@gmail.com>
2022-12-30 22:38:07 +0800

    added needed features

--- projects/todo/src/main.rs
+++ projects/todo/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Welcome to todo program!");
+    // desired features:
+        // sort todos
+        // create todos with deadline
+        // add additional info to todo
+        // simple syntax:
+            // ex: todo -new TODO_1 -deadline 2023/1/1 -add_info https://google.com, https://youtube.com
+        // use json file format
+        // list todos
+        // remind what todos have to be done that has deadline
+            // ex: 2022/12/31 do something
+        // auto remove todos after deadline date
 }

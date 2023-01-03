82688fd63dfb618697a015120598057b0a47d266
ivann <ivan.ng0515@gmail.com>
2023-01-03 21:16:27 +0800

    need to fin add info to file

--- projects/todo/src/main.rs
+++ projects/todo/src/main.rs
@@ -1,5 +1,8 @@
-use serde_json;
+use serde_json::{self, json};
+use std::fs::{File, OpenOptions};
 use std::io;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
     let mut usr_input = String::new();
@@ -22,32 +25,47 @@ fn main() {
                 todo_deadline: "".to_string(),
                 additional_info: "".to_string(),
             };
-            print!("Enter Todo name: ");
+            println!("Enter Todo name: ");
             io::stdin()
                 .read_line(&mut todo_info.todo_name)
                 .expect("Failed to read line");
-            print!("Enter Todo deadline: ");
+            println!("Enter Todo deadline: ");
             io::stdin()
                 .read_line(&mut todo_info.todo_deadline)
                 .expect("Failed to read line");
-            print!("Enter additonal notes: ");
+            println!("Enter additonal notes: ");
             io::stdin()
                 .read_line(&mut todo_info.additional_info)
                 .expect("Failed to read line");
             println!(
-                "Todo Info:\n\tName: {}\n\tDeadline: {}\n\tAdditional Info: {}",
+                "Todo Info:\tName: {}\tDeadline: {}\tAdditional Info: {}",
                 todo_info.todo_name, todo_info.todo_deadline, todo_info.additional_info
-            )
+            );
+            let mut json_obj = json!({});
+            json_obj["Todo name"] = json!(todo_info.todo_name);
+            json_obj["Todo deadline"] = json!(todo_info.todo_deadline);
+            json_obj["Additional Info"] = json!(todo_info.additional_info);
+            if Path::new("home").exists() {
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open("/home/todo.json")
+                    .unwrap();
+                writeln!(file, "{json_obj}").expect("Cannot Append to File");
+            } else if Path::new("home").exists() == false {
+                let mut file = File::create("/home/todo.json").unwrap();
+                file.write_all(serde_json::to_string(&object).unwrap().as_bytes())
+            }
         }
+        // desired features:
+        // sort todos
+        // create todos with deadline
+        // add additional info to todo
+        // use json file format
+        // list todos
+        // remind what todos have to be done that has deadline
+        // ex: 2022/12/31 do something
+        // auto remove todos after deadline date
+        // edit todos
     }
-    // desired features:
-    // sort todos
-    // create todos with deadline
-    // add additional info to todo
-    // use json file format
-    // list todos
-    // remind what todos have to be done that has deadline
-    // ex: 2022/12/31 do something
-    // auto remove todos after deadline date
-    // edit todos
 }

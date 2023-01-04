f04c626fabc8da34f91c88fbc73333e7e97fcf1b
ivann <ivan.ng0515@gmail.com>
2023-01-04 21:48:44 +0800

    todo new done

--- projects/todo/src/main.rs
+++ projects/todo/src/main.rs
@@ -38,23 +38,33 @@ fn main() {
                 .read_line(&mut todo_info.additional_info)
                 .expect("Failed to read line");
             println!(
-                "Todo Info:\tName: {}\tDeadline: {}\tAdditional Info: {}",
+                "Todo Info:\n\tName: {}\tDeadline: {}\tAdditional Info: {}",
                 todo_info.todo_name, todo_info.todo_deadline, todo_info.additional_info
             );
-            let mut json_obj = json!({});
-            json_obj["Todo name"] = json!(todo_info.todo_name);
-            json_obj["Todo deadline"] = json!(todo_info.todo_deadline);
-            json_obj["Additional Info"] = json!(todo_info.additional_info);
-            if Path::new("home").exists() {
+            let json_obj = json!(
+                {
+                    "1. Todo Name": todo_info.todo_name,
+                    "2. Todo Deadline": todo_info.todo_deadline,
+                    "3. Additional Notes": todo_info.additional_info,
+                }
+            );
+            if Path::new("./todo.json").exists() {
+                println!("append");
                 let mut file = OpenOptions::new()
                     .write(true)
                     .append(true)
-                    .open("/home/todo.json")
+                    .open("./todo.json")
                     .unwrap();
                 writeln!(file, "{json_obj}").expect("Cannot Append to File");
-            } else if Path::new("home").exists() == false {
-                let mut file = File::create("/home/todo.json").unwrap();
-                file.write_all(serde_json::to_string(&object).unwrap().as_bytes())
+            } else if Path::new("./todo.json").exists() == false {
+                println!("make new");
+                let _file = File::create("./todo.json").unwrap();
+                let mut file_opt = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open("./todo.json")
+                    .unwrap();
+                write!(file_opt, "{json_obj}\n").expect("Cannot Append to File");
             }
         }
         // desired features:
@@ -67,5 +77,6 @@ fn main() {
         // ex: 2022/12/31 do something
         // auto remove todos after deadline date
         // edit todos
+        // config options
     }
 }

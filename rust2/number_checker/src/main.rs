189c401fbeaa72c404305707e4116191a14e2a27
ivann <ivan.ng0515@gmail.com>
2023-02-19 10:51:54 +0800

    commit

--- rust2/number_checker/src/main.rs
+++ rust2/number_checker/src/main.rs
@@ -19,12 +19,35 @@ fn main() {
         if result % 2 == 0 {
             result /= 2;
             sequence_steps += 1;
-        } else if result % 2 != 0 {
+        } else {
             result = result * 3 + 1;
             sequence_steps += 1;
         }
     }
-    println!("This collatz sequence is {sequence_steps} steps")
+    println!("This collatz sequence is {sequence_steps} steps");
 
     // prime number checking
+    println!("Starting prime number checking");
+    
+    if user_input == 1 {
+        println!("Not prime, has 1 factor");
+    }
+
+    let mut pos_factors: i64 = 1;
+    let mut factors: u64 = 0;
+
+    while user_input > pos_factors {
+        if user_input % pos_factors == 0 {
+            factors += 1;
+            pos_factors += 1;
+        } else {
+            pos_factors += 1;
+        }
+    }
+    
+    if factors > 2 {
+        println!("Not prime, has {factors} factors");
+    } else if factors == 2{
+        println!("Is prime");
+    }
 }

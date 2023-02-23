70118f169e47f337daf55dc05b6a79ec961bb6db
ivann <ivan.ng0515@gmail.com>
2023-02-23 22:13:25 +0800

    commit

---
+++ rust2/grains/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut result: u64 = 1;
+    let mut squares: u32 = 1;
+
+    result *= 2;
+    while squares != 64 {
+        print!(" Grains {result} ");
+        if squares % 8 == 0 {
+            println!("");
+        }
+        result *= 2;
+        squares += 1;
+    }
+
+
+}

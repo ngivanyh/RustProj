65c866a66042e1973b4732320459c41bc53d21f1
ivann <ivan.ng0515@gmail.com>
2023-02-27 20:56:19 +0800

    added wc

---
+++ rust2/wc-simple/src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, fs};
+use text_colorizer::Colorize;
+
+fn main() -> std::io::Result<()>{
+    let args: Vec<String> = env::args().skip(1).collect();
+    
+    if args.len() != 1 {
+        eprintln!("{}", "Error: Expected 1, got more than one command line arguments".red().bold());
+        std::process::exit(1)
+    }
+    
+    let file_data = fs::metadata(&args[0])?;
+
+    println!("File size {}", file_data.len());
+
+    Ok(())
+}

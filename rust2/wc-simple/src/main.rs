34967ec4fdeb7e496ab6a5e0a84b3c8df16db266
ivann <ivan.ng0515@gmail.com>
2023-03-01 21:00:19 +0800

    commit

--- rust2/wc-simple/src/main.rs
+++ rust2/wc-simple/src/main.rs
@@ -1,20 +1,16 @@
 use std::{env, fs};
 use text_colorizer::Colorize;
 
-fn main() -> std::io::Result<()>{
+fn main() { 
     let args: Vec<String> = env::args().skip(1).collect();
     
     if args.len() != 1 {
         eprintln!("{}", "Error: Expected 1, got more than one command line arguments".red().bold());
-        std::process::exit(1)
+        std::process::exit(1);
     }
     
-    let file_data = fs::metadata(&args[0])?;
+    let contents = fs::read_to_string(&args[0]).expect("Failed to read contents");
 
-    println!("File size {}", file_data.len());
-   
-    let contents = fs::read_to_string(&args[0]);
-
+    let mut words: Vec<&str> = contents.split_whitespace().collect();
     
-    Ok(())
-}
+

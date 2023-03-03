420c9e4421efb54558752122c18e17c5570c4e90
ivann <ivan.ng0515@gmail.com>
2023-03-03 20:47:21 +0800

    consider it done

--- rust2/wc-simple/src/main.rs
+++ rust2/wc-simple/src/main.rs
@@ -18,17 +18,17 @@ fn main() {
     let mut words_looped = 0;
     let mut wc = 0;
 
-    while words_looped != words.len() {
-        let mut n_word = words_looped + 1;
+    while words_looped != words.len() - 1 {
         wc = 0;
-        if words[words_looped] == words[n_word] {
+        let mut n_word = words_looped + 1;
+        if words[words_looped] == words[n_word] && n_word != words.len() - 1 && words_looped != words.len() - 1 {
             wc += 2;
+            words_looped += 1;
             n_word += 1;
-            while words[n_word] == words[words_looped] {
+            while n_word != words.len() - 1 && words_looped != words.len() - 1 && words[n_word] == words[words_looped] {
                 wc += 1;
-                n_word += 1;
                 words_looped += 1;
-
+                n_word += 1;
             }
             println!("Word: {}, frequency: {wc}", words[words_looped]);
         }

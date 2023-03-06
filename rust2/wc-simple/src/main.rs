ad57c27fb4bb04da86eb0dce27f058327e8bd1a2
ivann <ivan.ng0515@gmail.com>
2023-03-06 21:06:43 +0800

    commit

--- rust2/wc-simple/src/main.rs
+++ rust2/wc-simple/src/main.rs
@@ -1,6 +1,15 @@
 use std::{env, fs};
 use text_colorizer::Colorize;
 
+
+fn hasNext(v: Vec<String>, current_word: i32) -> bool{
+    if current_word + 1 == (v.len() - 1 as i32) {
+        true
+    } else {
+        false
+    }
+}
+
 fn main() { 
     let args: Vec<String> = env::args().skip(1).collect();
     
@@ -15,27 +24,27 @@ fn main() {
 
     words.sort();
 
-    let mut words_looped = 0;
+    let mut cur_word: usize = 0;
     let mut wc = 0;
     
     println!("{:?}", words);
     
-    while words_looped != words.len() - 1 {
+    while cur_word != words.len() - 1 {
         wc = 0;
-        let mut n_word = words_looped + 1;
-        if words[words_looped] == words[n_word] && n_word != words.len() - 1 && words_looped != words.len() - 1 {
+        let mut n_word = cur_word + 1;
+        if words[cur_word] == words[n_word] && n_word != words.len() - 1 && cur_word != words.len() - 1 {
             wc += 2;
-            words_looped += 1;
+            cur_word += 1;
             n_word += 1;
-            while words_looped != words.len() - 1 && words[n_word] == words[words_looped] {
-                words_looped += 1;
+            while cur_word != words.len() - 1 && words[n_word] == words[cur_word] {
+                cur_word += 1;
                 wc += 1;
                 n_word += 1;
                 
             }
-            println!("Word: {}, frequency: {wc}", words[words_looped]);
+            println!("Word: {}, frequency: {wc}", words[cur_word]);
         }
 
-        words_looped += 1;
+        cur_word += 1;
     }
 } 

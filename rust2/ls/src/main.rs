1b6d1983f611a081ad5e8ba231488013ffbb75b8
ivann <ivan.ng0515@gmail.com>
2023-03-09 22:35:28 +0800

    commit

--- rust2/ls/src/main.rs
+++ rust2/ls/src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, fs};
+use text_colorizer::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    
+    if args.len() != 1 {
+        eprintln!("{}", "Error colleting args".red().bold());
+        std::process::exit(1);
+    }
+
+    println!("path: {}", args[0]);
+
+    let mut dirs: Vec<&str> = Vec::new();
+
+    for file in fs::read_dir(&args[0]).unwrap() {
+        dirs.push(file.unwrap().path().display().to_string());
+        println!("{}", file.unwrap().path().display());
+    }
 }

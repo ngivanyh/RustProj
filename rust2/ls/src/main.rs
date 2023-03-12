d5572019aa055d357592c04a3222350ffd472e99
ivann <ivan.ng0515@gmail.com>
2023-03-12 22:27:36 +0800

    need more changes, but works

--- rust2/ls/src/main.rs
+++ rust2/ls/src/main.rs
@@ -9,12 +9,20 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("path: {}", args[0]);
-
-    let mut dirs: Vec<&str> = Vec::new();
+    let mut dirs: Vec<String> = Vec::new();
 
     for file in fs::read_dir(&args[0]).unwrap() {
         dirs.push(file.unwrap().path().display().to_string());
-        println!("{}", file.unwrap().path().display());
     }
+    dirs.sort();
+
+    for dir in dirs {
+        let new_dir = dir.replace(&args[0], "");
+        if &new_dir[0..2] == "/." {
+            println!("{}", new_dir.bold());
+        } else {
+            println!("{}", new_dir.cyan().bold());
+        }
+    }
+
 }

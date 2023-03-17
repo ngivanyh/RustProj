d9df497fe6075318ac9fc38fd0eeccc42383ac6a
ivann <ivan.ng0515@gmail.com>
2023-03-17 22:01:59 +0800

    commit

--- rust2/rust-shortcuts/src/lib.rs
+++ rust2/rust-shortcuts/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn is_odd(val: isize) -> bool {
-    if val % 2 != 0 {
-        true
-    } else {
-        false
+mod shortcuts {
+    pub fn is_odd(val: isize) -> bool {
+        if val % 2 != 0 {
+            true
+        } else {
+            false
+        }
     }
 }
 
-
 #[test]
 fn test_odd() {
-    assert!(is_odd(1) == true);
-    assert!(is_odd(2) == false);    
+    assert!(shortcuts::is_odd(1) == true);
+    assert!(shortcuts::is_odd(2) == false);    
 }

b01a244dec78c9d621ccbfabb2e722bf74e8fbd0
ivann <ivan.ng0515@gmail.com>
2023-03-23 20:54:06 +0800

    commit in a long time

--- rust2/navfs/src/ls.rs
+++ rust2/navfs/src/ls.rs
@@ -1,17 +1,7 @@
 use std::{env, fs};
 use std::path::PathBuf;
-use text_colorizer::Colorize;
 
-fn filetype(dir: &str, print_dir: &str) {
-    let path = PathBuf::from(dir);
-    if  path.is_dir() {
-        println!("{}", print_dir.blue().bold());
-    } else {
-        println!("{}", print_dir.bold());
-    }
-}
-
-pub fn ls(mut path: Option<String>) {    
+pub fn ls(mut path: Option<String>) -> Vec<String> {    
     if path == None {
         match env::var("PWD") {
             Ok(val) => path = Some(val.to_string()),
@@ -26,11 +16,6 @@ pub fn ls(mut path: Option<String>) {
     for file in fs::read_dir(&new_path).unwrap() {
         dirs.push(file.unwrap().path().display().to_string());
     }
-    dirs.sort();
     
-    for dir in dirs {
-        let new_dir = dir.replace(&new_path, "");
-        filetype(&dir, &new_dir);
-    }
-
+    dirs
 }
